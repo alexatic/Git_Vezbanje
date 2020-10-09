cc88ef09f06e2123e190a59d77829b1296503c13
Aleksandar Atic <aleksandar.atic92@gmail.com>
2020-10-09 14:51:49 +0200

    Initial commit

---
+++ range.py
@@ -0,0 +1,18 @@
+def range (start,end):
+    current = start
+    while current < end:
+        yield current
+        current +=1
+def sentence(sentence):
+    for word in sentence.split():
+        yield word
+
+
+my_sentence = sentence ('This is a test fuck me')
+for word in my_sentence:
+    print(word )
+
+brojevi = range(1,100)
+
+for i in brojevi:
+    print(i)
