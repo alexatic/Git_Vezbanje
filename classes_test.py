9cd5797706cb422126bb26a53faf76be18c76d86
Aleksandar Atic <aleksandar.atic92@gmail.com>
2020-10-09 14:58:53 +0200

    Add classes_test.py

---
+++ classes_test.py
@@ -0,0 +1,29 @@
+class Hero:
+    '''
+
+A hero who is allergit to peanuts
+
+    '''
+    def __init__(self,name):
+        self.name = name
+        self.health = 100
+    def eat(self,food):
+        if(food == 'peanut'):
+            self.health -=100
+        elif (food == 'apple'):
+            self.health +=20
+
+Boban = Hero('Boban')
+print (Boban.name)
+print (Boban.health)
+Boban.eat('apple')
+print (Boban.health)
+Boban.eat ('peanut')
+print (Boban.health)
+input()
+
+
+
+
+
+
